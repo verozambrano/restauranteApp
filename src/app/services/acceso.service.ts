a1ee99514b07a3335c722a97886f3a4ab4ddb731
verozambrano <vrnc098@gmail.com>
2024-09-14 09:01:20 -0500

    crear

---
+++ src/app/services/acceso.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccesoService {
+
+  constructor(private http:HttpClient) { }
+  private="http://localhost:3000/usuarios"
+  getUsuarios():Observable <any>{
+   return this.http.get(this.API_USER)
+  }
+  
+  postUser():Observable<any>{
+    return this.http.post(this.API_Us.Mesero)
+  }
+}
